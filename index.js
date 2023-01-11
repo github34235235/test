31296aa402f8943a98551faf34665e16da7d6477
Bob <collegians_taverners@simplelogin.com>
2023-01-11 21:23:51 +0200

    init

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var fs = require('fs');
 
 var html = fs.readFileSync('index.html');
 
-const PORT = process.env.PORT || 8000;
+process.env.PORT = 8000;
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(html);
-}).listen(PORT, () => console.log("Server is listening on port " + PORT + "..."));
+}).listen(process.env.PORT, () => console.log("Server is listening on port " + process.env.PORT + "..."));
