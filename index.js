f3247502746ff5c37249c5edef4df6986ad3d40e
Bob <collegians_taverners@simplelogin.com>
2023-01-11 20:54:51 +0200

    init

--- index.js
+++ index.js
@@ -6,6 +6,4 @@ var html = fs.readFileSync('index.html');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(html);
-}).listen(process.env.PORT);
-
-process.stdout.write("port: " + process.env.PORT);
+}).listen(8000);
